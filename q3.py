e6375e8ed25dc1f0106cca7fff6c6e4b2190c6f4
Shwetank <shwetankt88@gmail.com>
2025-06-12 16:19:25 +0530

    Formatted and renamed files for better understanding

--- q3.py
+++
@@ -1,4 +0,0 @@
-l= ["apple", "bat", "banana","dog"]
-sorted_words = sorted(l, key=len)
-
-print(sorted_words) 
--- questions/q1.py
+++
@@ -1,9 +0,0 @@
-set1 = {"apple", "banana", "cherry"}
-set2 = {1, 5, 7, 9, 3}
-set3 = {True, False, False}
-set1.pop()
-set2.pop()
-set3.pop()
-print(set1)
-print(set2)
-print(set3)
