9730ea8899a6fac722e7f83000d911f4f869a35a
Shwetank <shwetankt88@gmail.com>
2025-07-18 15:01:11 +0530

    practice commit

--- unique_words_in_string.py
+++ unique_words_in_string.py
@@ -7,6 +7,6 @@ def unique_words_in_string(str):
             temp_set.add(i)# adding non duplicate elements
     return len(temp_set)#gives length
 print(unique_words_in_string("this is a test this is only a test"))
-#edited
+
 
 
